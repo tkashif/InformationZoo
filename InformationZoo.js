27afb43dde46cee69558b23e88c7be02acb4738a
taimurkashif <taimurusca@gmail.com>
2020-03-21 12:55:41 -0700

    Drop down adding animal name working
    Adding name working, but still need to fix issue with when we first load up page

--- InformationZoo.js
+++ InformationZoo.js
@@ -1,6 +1,30 @@
-$(function(){
-  $("SelectAnimal").select(function(){
-   	// do stuff 
-  });
+function AnimalSelected() {
+ var animalList = document.getElementById("AnimalList");
+var selectedAnimalIndex = animalList.selectedIndex;    
+var animalListArray = new Array();
+for (i = 0; i < animalList.options.length; i++) {
+    animalListArray[i] = animalList.options[i].value;
+}
+    
+ var animalSelected = animalListArray[selectedAnimalIndex];    
+ var firstLetter = animalSelected[0];
+ var outputSelection = "";
+ var outputLearnMore = "" ;
+ if (LetterIsVowel(firstLetter)) {
+        outputSelection = "You Selected an " + animalSelected;
+        outputLearnMore = "Learn More About an " + animalSelected;
+    }
+    else {
+        outputSelection = "You Selected a " + animalSelected;
+        outputLearnMore = "Learn More About a " + animalSelected;
+    }
+    
+    
+    document.getElementById("YouSelected").innerHTML = outputSelection;
+    document.getElementById("LearnMoreAbout").innerHTML = outputLearnMore;
+    
+}
 
-});
+function LetterIsVowel(letter) {
+    return letter == "A" || letter == "E" || letter == "I" || letter == "O" || letter == "U"
+}
