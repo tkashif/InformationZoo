73ad475e795f56fd8c95ac9d2ef7c88f04e98f3c
taimurkashif <taimurusca@gmail.com>
2020-06-14 12:58:25 -0700

    JS Beautifier
    Used JS Beautifier to format code

--- InformationZoo.js
+++ InformationZoo.js
@@ -3,94 +3,76 @@ function FirstOpened() {
 }
 
 function AnimalSelected() {
- var animalList = document.getElementById("AnimalList");
-var selectedAnimalIndex = animalList.selectedIndex;    
-var animalListArray = new Array();
-for (i = 0; i < animalList.options.length; i++) {
-    animalListArray[i] = animalList.options[i].value;
-}
-    
-if (selectedAnimalIndex != 0) { 
-var animalSelected = animalListArray[selectedAnimalIndex];    
- var firstLetter = animalSelected[0];
- var outputSelection = "";
- var outputLearnMore = "" ;
-var outputArticlesAbout = "";
- if (LetterIsVowel(firstLetter)) {
-        outputSelection = "You Selected an " + animalSelected;
-        outputLearnMore = "Learn More About an " + animalSelected;
-        outputArticlesAbout = "Articles About an " + animalSelected;
-        
+    var animalList = document.getElementById("AnimalList");
+    var selectedAnimalIndex = animalList.selectedIndex;
+    var animalListArray = new Array();
+    for (i = 0; i < animalList.options.length; i++) {
+        animalListArray[i] = animalList.options[i].value;
     }
-    else {
-        outputSelection = "You Selected a " + animalSelected;
-        outputLearnMore = "Learn More About a " + animalSelected;
-        outputArticlesAbout = "Articles About a " + animalSelected;
+
+    if (selectedAnimalIndex != 0) {
+        var animalSelected = animalListArray[selectedAnimalIndex];
+        var firstLetter = animalSelected[0];
+        var outputSelection = "";
+        var outputLearnMore = "";
+        var outputArticlesAbout = "";
+        if (LetterIsVowel(firstLetter)) {
+            outputSelection = "You Selected an " + animalSelected;
+            outputLearnMore = "Learn More About an " + animalSelected;
+            outputArticlesAbout = "Articles About an " + animalSelected;
+
+        } else {
+            outputSelection = "You Selected a " + animalSelected;
+            outputLearnMore = "Learn More About a " + animalSelected;
+            outputArticlesAbout = "Articles About a " + animalSelected;
+        }
+
+
+        document.getElementById("YouSelected").innerHTML = outputSelection;
+        document.getElementById("LearnMoreAbout").innerHTML = outputLearnMore;
+        document.getElementById("ArticlesAbout").innerHTML = outputArticlesAbout;
+
+        MakeVisible();
+        Wikipedia(animalSelected);
+        Newspaper(animalSelected);
     }
-    
-    
-    document.getElementById("YouSelected").innerHTML = outputSelection;
-    document.getElementById("LearnMoreAbout").innerHTML = outputLearnMore;
-    document.getElementById("ArticlesAbout").innerHTML = outputArticlesAbout;
-    
-    MakeVisible();
-    Wikipedia(animalSelected);
-    Newspaper(animalSelected);
-}
-    
+
 }
 
 function LetterIsVowel(letter) {
     return letter == "A" || letter == "E" || letter == "I" || letter == "O" || letter == "U"
 }
 
-function Wikipedia(selectedAnimal){
-   var http = new XMLHttpRequest();
+function Wikipedia(selectedAnimal) {
+    var http = new XMLHttpRequest();
     var url = GenerateWikiURL(selectedAnimal);
-    
-http.onreadystatechange=function(){
-   if (this.readyState == 4 && this.status == 200) {
-       var data = JSON.parse(this.response);
-        document.getElementById("AnimalInfo").innerHTML = data["parse"]["text"];
-        MakeWikiLinksClickable();   
-   } 
-}
 
-http.open('GET', url, true);
-http.send();
-     
+    http.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.response);
+            document.getElementById("AnimalInfo").innerHTML = data["parse"]["text"];
+            MakeWikiLinksClickable();
+        }
+    }
+
+    http.open('GET', url, true);
+    http.send();
+
 
 }
 
 function GenerateWikiURL(selectedAnimal) {
-    return 'https://en.wikipedia.org/w/api.php?action=parse&page=' + formatAnimal(selectedAnimal) + '&prop=text&formatversion=2&format=json&origin=*&section=0';   
+    return 'https://en.wikipedia.org/w/api.php?action=parse&page=' + formatAnimal(selectedAnimal) + '&prop=text&formatversion=2&format=json&origin=*&section=0';
 }
 
 function MakeWikiLinksClickable() {
-    /*console.log("Hostname: " + location.host);
-    var hostLength = location.host.length;
-    var httpsLength = "http://".length;
-    var linkStartIndex = hostLength + httpsLength;
-    var wikiLinks = document.getElementById("AnimalInfo").querySelectorAll('a');
-    var wikiNotChangeLinks = document.getElementsByClassName("external text");
-    var compareIndex = 0;
-    for (i = 0; i < wikiLinks.length; i++) {
-        if (wikiNotChangeLinks[compareIndex] != wikiLinks[i]) {
-            var fullLink = wikiLinks[i].href;
-            var endOfLink = fullLink.substring(linkStartIndex, fullLink.length);
-            wikiLinks[i].href = 'https://en.wikipedia.org' + endOfLink;
-        } else {
-            compareIndex++;
-        }
-    } */
-    
     var hostLength = location.host.length;
     var httpsLength = "http://".length;
     var linkStartIndex = hostLength + httpsLength;
     var wikiLinks = document.getElementsByTagName('a');
     for (i = 0; i < wikiLinks.length; i++) {
         console.log(wikiLinks[i].href);
-        if(wikiLinks[i].href.includes("/wiki/") || wikiLinks[i].href.includes("/w/")) {
+        if (wikiLinks[i].href.includes("/wiki/") || wikiLinks[i].href.includes("/w/")) {
             var fullLink = wikiLinks[i].href;
             var endOfLink = fullLink.substring(linkStartIndex, fullLink.length);
             wikiLinks[i].href = 'https://en.wikipedia.org' + endOfLink;
@@ -104,7 +86,7 @@ function formatAnimal(selectedAnimal) {
     for (i = 0; i < splitSelectedAnimalArray.length; i++) {
         formattedAnimal += splitSelectedAnimalArray[i];
         if (i != splitSelectedAnimalArray.length - 1) {
-            formattedAnimal += "_"; 
+            formattedAnimal += "_";
         }
     }
     return formattedAnimal;
@@ -113,48 +95,48 @@ function formatAnimal(selectedAnimal) {
 function Newspaper(selectedAnimal) {
     var http = new XMLHttpRequest();
     var url = GenerateNewspaperURL(selectedAnimal);
-    
-http.onreadystatechange=function(){
-   if (this.readyState == 4 && this.status == 200) {
-        var data = JSON.parse(this.response);
-        data = data["items"];
-       var htmlString = ""
-       for (i = 0; i < data.length; i++) {
-           currentData = data[i];
-           title = currentData["title"];
-           placeOfPublication = currentData["place_of_publication"];
-           rawUrl = currentData["url"];
-           // getting rid of .json
-           url = rawUrl.substring(0, rawUrl.length - 5)
-           htmlString += "<section class = articleBlurb>" 
-           htmlString += "Article #".bold().big() + (i+1).toString().bold().big() + "<br>"
-           htmlString += "Title: " + title + "<br>";
-           htmlString += "Place of publication: " + placeOfPublication + "<br>";
-           htmlString += "URL: " + "<a href = " + url + " target = _blank" + ">" + url + "</a>";
-           htmlString += "</section>"
 
-           
-       }
-        document.getElementById("Newspaper").innerHTML = htmlString;
-   } 
-}
+    http.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.response);
+            data = data["items"];
+            var htmlString = ""
+            for (i = 0; i < data.length; i++) {
+                currentData = data[i];
+                title = currentData["title"];
+                placeOfPublication = currentData["place_of_publication"];
+                rawUrl = currentData["url"];
+                // getting rid of .json
+                url = rawUrl.substring(0, rawUrl.length - 5)
+                htmlString += "<section class = articleBlurb>"
+                htmlString += "Article #".bold().big() + (i + 1).toString().bold().big() + "<br>"
+                htmlString += "Title: " + title + "<br>";
+                htmlString += "Place of publication: " + placeOfPublication + "<br>";
+                htmlString += "URL: " + "<a href = " + url + " target = _blank" + ">" + url + "</a>";
+                htmlString += "</section>"
 
-http.open('GET', url, true);
-http.send();
 
-    
+            }
+            document.getElementById("Newspaper").innerHTML = htmlString;
+        }
+    }
+
+    http.open('GET', url, true);
+    http.send();
+
+
 }
 
 function GenerateNewspaperURL(selectedAnimal) {
     return "https://chroniclingamerica.loc.gov/search/titles/results/?terms=" + formatAnimal(selectedAnimal) + "&format=json";
-    
+
 }
 
-function MakeVisible(){
+function MakeVisible() {
     var classElements = document.getElementsByClassName("invisible");
-    
+
     for (i = 0; i < classElements.length; i++) {
         classElements[i].style.visibility = "visible";
     }
-    
+
 }
